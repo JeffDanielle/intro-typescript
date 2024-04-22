6761d29444e2aa1f6c712697c3ef5a816c44d98e
JeffDanielle <bayotjeff@gmail.com>
2024-04-22 15:31:13 +0800

    16. Object Types

--- app.ts
+++ app.ts
@@ -1,22 +1,15 @@
-// Specify the types of the parameters/variable to expect a specific data type
-// Key difference: JavaScript uses dynamic types resolved at run-time meanwhile TypeScript uses static types set during development
-const add = (
-    num1: number,
-    num2: number,
-    showResult: boolean,
-    phase: string
-) => {
-    if (showResult) {
-        console.log(`${phase} ${num1 + num2}`);
-        console.log(typeof phase, typeof num1, typeof num2);
-    } else {
-        return num1 + num2;
-    }
+/*
+ !! OPTIONAL SUB OPTIMAL CODE BELOW !!
+ Provide key value pairs if you want to explicitly define the properties of the object. this will allow
+ us expect what properties will be return in an object
+*/
+// const person: {
+//     name: string;
+//     age: number;
+// } = {
+const person = {
+    name: "Jeff",
+    age: 24,
 };
 
-const number1 = 3;
-const number2 = 10;
-const printResult = true;
-const resultPhrase = "Result is:";
-
-add(number1, number2, printResult, resultPhrase);
+console.log(person.name);
---
+++ basics.ts
@@ -0,0 +1,22 @@
+// Specify the types of the parameters/variable to expect a specific data type
+// Key difference: JavaScript uses dynamic types resolved at run-time meanwhile TypeScript uses static types set during development
+const add = (
+    num1: number,
+    num2: number,
+    showResult: boolean,
+    phase: string
+) => {
+    if (showResult) {
+        console.log(`${phase} ${num1 + num2}`);
+        console.log(typeof phase, typeof num1, typeof num2);
+    } else {
+        return num1 + num2;
+    }
+};
+
+const number1 = 3;
+const number2 = 10;
+const printResult = true;
+const resultPhrase = "Result is:";
+
+add(number1, number2, printResult, resultPhrase);
