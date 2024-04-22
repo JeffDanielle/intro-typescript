d7dce02bb5433e84e776c0a6f4b479aa5a145c40
JeffDanielle <bayotjeff@gmail.com>
2024-04-22 14:52:19 +0800

    14. Working with Numbers, Strings & Booleans

--- app.ts
+++ app.ts
@@ -1,10 +1,22 @@
 // Specify the types of the parameters/variable to expect a specific data type
 // Key difference: JavaScript uses dynamic types resolved at run-time meanwhile TypeScript uses static types set during development
-const add = (num1: number, num2: number) => {
-    return num1 + num2;
+const add = (
+    num1: number,
+    num2: number,
+    showResult: boolean,
+    phase: string
+) => {
+    if (showResult) {
+        console.log(`${phase} ${num1 + num2}`);
+        console.log(typeof phase, typeof num1, typeof num2);
+    } else {
+        return num1 + num2;
+    }
 };
 
 const number1 = 3;
 const number2 = 10;
+const printResult = true;
+const resultPhrase = "Result is:";
 
-console.log(add(number1, number2));
+add(number1, number2, printResult, resultPhrase);
