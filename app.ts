414565bba137fb1c3d6d454dcaf8f458c7695ede
JeffDanielle <bayotjeff@gmail.com>
2024-04-22 14:27:55 +0800

    11. Using Types

---
+++ app.ts
@@ -0,0 +1,9 @@
+// Specify the types of the parameters/states to expect a specific data type
+const add = (num1: number, num2: number) => {
+    return num1 + num2;
+};
+
+const number1 = 3;
+const number2 = 10;
+
+console.log(add(number1, number2));
