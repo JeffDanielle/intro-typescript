a26a921ea09f62f595c48cd2a7a830b5aeb99f75
JeffDanielle <bayotjeff@gmail.com>
2024-04-22 17:08:54 +0800

    23. Literal Types

--- app.ts
+++ app.ts
@@ -1,16 +1,33 @@
 //  Use pipe for union types '|' if we expected two or more data types
-function combine(input1: number | string, input2: number | string) {
+// Literal types are based on core types but with specific version: example(resultConversion)
+function combine(
+    input1: number | string,
+    input2: number | string,
+    resultConversion: "as-number" | "as-text"
+) {
     let result;
-    if (typeof input1 === "number" && typeof input2 === "number") {
-        result = input1 + input2;
+    if (
+        (typeof input1 === "number" && typeof input2 === "number") ||
+        resultConversion === "as-number"
+    ) {
+        result = +input1 + +input2;
     } else {
         result = input1.toString() + input2.toString();
     }
+    // if (resultConversion === "as-number") {
+    //     return +result;
+    // } else {
+    //     return result.toString();
+    // }
     return result;
 }
 
-const combinedAges = combine(30, 26);
+// Literal types
+const combinedAges = combine(30, 26, "as-number");
 console.log(combinedAges);
 
-const combineNames = combine("Max", "Anna");
+const combinedStringAges = combine("30", "26", "as-number");
+console.log(combinedStringAges);
+
+const combineNames = combine("Max", "Anna", "as-text");
 console.log(combineNames);
