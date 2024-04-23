9bf49e02742142c249afb3e54a01ae4f941cdec0
JeffDanielle <bayotjeff@gmail.com>
2024-04-23 09:34:46 +0800

    28. Function Types & Callbacks

--- app.ts
+++ app.ts
@@ -7,6 +7,12 @@ function printResults(num: number) {
     console.log("Result: " + num);
 }
 
+// Function callbacks
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+    const result = n1 + n2;
+    cb(result);
+}
+
 printResults(adds(5, 12));
 
 // Function dataType
@@ -19,3 +25,7 @@ combineValues = adds;
 // combineValues = 5;
 console.log(combineValues(8, 8));
 //  let someValue: undefined
+
+addAndHandle(10, 20, (weh) => {
+    console.log(weh);
+});
